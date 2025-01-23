a829351d1f37780d3a356a18ca51a1fe20b64fc5
Kevin Bernal <kevinbernal@canva.com>
2025-01-23 16:47:01 +1100

    minimal reproducible example

---
+++ test.py
@@ -0,0 +1,9 @@
+# Importing any of these would result in coverage issues ❌
+# import torchvision
+# from transformers.models.distilbert import DistilBertModel
+
+from main import foo
+
+
+def test_foo():
+    assert foo() == "bar"
